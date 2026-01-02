546e6e7929d9ed78684cd6294af59825a091c4a8
GouravMalviya445 <gouravmalviya445@gmail.com>
2026-01-02 23:23:25 +0530

    add HTTP server setup with graceful shutdown and logging

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,57 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gouravmalviya445/event-booking-golang/internal/config"
 )
 
 func main() {
-	fmt.Println("Event booking service...")
+	// load config
+	cfg := config.MustLoad()
+
+	// database setup
+
+	// setup router
+	r := http.NewServeMux()
+
+	r.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome to event booking service"))
+	})
+
+	// setup server
+	srv := http.Server{
+		Addr:    cfg.HTTPServer.Addr,
+		Handler: r,
+	}
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+
+	go func(){
+		slog.Info("Server started", slog.String("address", srv.Addr))
+		if err := srv.ListenAndServe(); err != nil {
+			log.Fatal("Failed to start server")
+		}
+	}()
+
+	<-sig // block until signal not received
+
+	// graceful shutdown
+	slog.Info("Shutting down the server...")
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second * 5)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		slog.Error("Failed to shutdown server", slog.String("error", err.Error()))
+	}
+
+	slog.Info("Server shutdown successfully")
 }
