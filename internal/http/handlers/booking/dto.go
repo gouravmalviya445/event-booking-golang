9a9e5151e3a7d10de708f9c6404c987c06a4877b
GouravMalviya445 <gouravmalviya445@gmail.com>
2026-01-19 00:40:43 +0530

    refactor Booking to BookingOrder and add BookingVerify and BookingWebhook DTOs

--- internal/http/handlers/booking/dto.go
+++ internal/http/handlers/booking/dto.go
@@ -2,9 +2,31 @@ package booking
 
 // DTO -> Data transfer objects
 
-type Booking struct {
-	UserId string `json:"userId" validate:"required"`
-	EventId string `json:"eventId" validate:"required"`
+type BookingOrder struct {
+	UserId   string `json:"userId" validate:"required"`
+	EventId  string `json:"eventId" validate:"required"`
+	Amount   int64  `json:"amount" validate:"required"`
+	Currency string `json:"currency" validate:"required"`
+}
 
-	// TODO: add payment later
+type BookingVerify struct {
+	RazorpayPaymentId string `json:"razorpay_payment_id" validate:"required"`
+	RazorpayOrderId   string `json:"razorpay_order_id" validate:"required"`
+	RazorpaySignature string `json:"razorpay_signature" validate:"required"`
+}
+
+
+// webhook data send by the razorpay in POST request to notify on event
+type BookingWebhook struct {
+	Event   string `json:"event" validate:"required"`
+	Payload struct {
+		Payment struct {
+			Entity struct {
+				ID       string `json:"id" validate:"required"`       // payment_id
+				OrderID  string `json:"order_id" validate:"required"` // order_id
+				Status   string `json:"status" validate:"required"`   // authorized / captured / failed
+				Captured bool   `json:"captured" validate:"required"` // true only when captured
+			} `json:"entity" validate:"required"`
+		} `json:"payment" validate:"required"`
+	} `json:"payload" validate:"required"`
 }
