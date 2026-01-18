7d95006ec00df45b47a1fa997a60e6e41bc1f3ec
GouravMalviya445 <gouravmalviya445@gmail.com>
2026-01-18 22:12:38 +0530

    refactor booking model add feilds for razorpay information

--- internal/models/booking.go
+++ internal/models/booking.go
@@ -7,12 +7,21 @@ import (
 )
 
 type Booking struct {
-	ID         bson.ObjectID `bson:"_id,omitempty" json:"_id"`
-	UserID     bson.ObjectID `bson:"userId" json:"userId"`
-	EventID    bson.ObjectID `bson:"eventId" json:"eventId"`
-	Tickets    int           `bson:"tickets" json:"tickets"`
-	TotalPrice float64       `bson:"totalPrice" json:"totalPrice"`
-	Status     string        `bson:"status" json:"status"`
-	CreatedAt  time.Time     `bson:"createdAt" json:"createdAt"`
-	UpdatedAt  time.Time     `bson:"updatedAt" json:"updatedAt"`
+	ID      bson.ObjectID `bson:"_id,omitempty" json:"_id"`
+	UserID  bson.ObjectID `bson:"userId" json:"userId"`
+	EventID bson.ObjectID `bson:"eventId" json:"eventId"`
+
+	Tickets    int     `bson:"tickets" json:"tickets"`
+	TotalPrice int `bson:"totalPrice" json:"totalPrice"`
+	Status     string  `bson:"status" json:"status"`
+
+	RazorpayOrderID   string `bson:"razorpayOrderId" json:"razorpay_order_id"`
+	RazorpayPaymentID string `bson:"razorpayPaymentId" json:"razorpay_payment_id"`
+	RazorpaySignature string `bson:"razorpaySignature" json:"razorpay_signature"`
+
+	Currency string `bson:"currency" json:"currency"`
+
+	CreatedAt time.Time `bson:"createdAt" json:"createdAt"`
+	UpdatedAt time.Time `bson:"updatedAt" json:"updatedAt"`
+	ExpiredAt time.Time `bson:"expiredAt" json:"expiredAt"`
 }
