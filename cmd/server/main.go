39a5e0bf8b67b4d156e51f105c0f4af32f2a735a
GouravMalviya445 <gouravmalviya445@gmail.com>
2026-01-25 23:58:24 +0530

    add router to see access  bookies

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -48,7 +48,8 @@ func main() {
 	r.HandleFunc("POST /api/bookings/order", booking.Initiate(storage, payment))
 	r.HandleFunc("POST /api/bookings/verify", booking.VerifyPayment(storage, payment))
 	r.HandleFunc("POST /api/bookings/confirm", booking.ConfirmBooking(storage, payment))
-	r.HandleFunc("GET /api/bookings", booking.CheckBookingStatus(storage))
+	r.HandleFunc("GET /api/bookings/{id}", booking.CheckBookingStatus(storage))
+	r.HandleFunc("GET /api/bookings", booking.GetBookings(storage))
 
 	// setup server
 	srv := http.Server{
