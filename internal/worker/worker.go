2a2d47c77b7c1acf20c34bfccb92a24988f57bb3
GouravMalviya445 <gouravmalviya445@gmail.com>
2026-01-27 20:41:23 +0530

    refactor ticket releasing background worker logic

--- internal/worker/worker.go
+++ internal/worker/worker.go
@@ -3,6 +3,7 @@ package worker
 import (
 	"context"
 	"log"
+	"log/slog"
 	"time"
 
 	"github.com/gouravmalviya445/event-booking-golang/internal/models"
@@ -26,27 +27,65 @@ func Worker(storage *mongodb.MongoDB, duration time.Duration, ctx context.Contex
 }
 
 func releaseTicket(storage *mongodb.MongoDB) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
 	eventCollection := storage.Db.Collection("events")
 	bookingCollection := storage.Db.Collection("bookings")
 
-	filter := bson.M{
-		"status": "pending",
-		"expiredAt": bson.M{
-			"$lt": time.Now().UTC(),
-		},
+	session, err := storage.Client.StartSession(options.Session())
+	defer session.EndSession(ctx)
+	if err != nil {
+		slog.Error("BgWorker: mongodb Session error", slog.String("err", err.Error()))
+		return
 	}
-	update := bson.M{
-		"$set": bson.M{
-			"status":    "expired",
-			"updatedAt": time.Now().UTC(),
-		},
+	slog.Info("background worker running...")
+
+	_, err = session.WithTransaction(ctx, func(ctx context.Context) (any, error) {
+		filter := bson.M{
+			"status": "pending",
+			"expiredAt": bson.M{
+				"$lt": time.Now().UTC(),
+			},
+		}
+		update := bson.M{
+			"$set": bson.M{
+				"status":    "expired",
+				"updatedAt": time.Now().UTC(),
+			},
+		}
+
+		var booking models.Booking
+		bookingResult := bookingCollection.
+			FindOneAndUpdate(
+				context.Background(),
+				filter,
+				update,
+				options.FindOneAndUpdate().SetReturnDocument(options.After),
+			)
+		if bookingResult.Err() != nil {
+			return nil, err
+		}
+		bookingResult.Decode(&booking)
+
+		var event models.Event
+		eventResult := eventCollection.
+			FindOneAndUpdate(
+				context.Background(),
+				bson.M{"_id": booking.EventID},
+				bson.M{"$inc": bson.M{"availableSeats": booking.Tickets}},
+			)
+		if eventResult.Err() != nil {
+			return nil, err
+		}
+		eventResult.Decode(&event)
+
+		return event.ID, nil
+	})
+
+	if err != nil {
+		slog.Error("Error while releasing ticket in background", slog.String("err", err.Error()))
+		return
 	}
 
-	log.Println("background worker...")
-	var booking models.Booking
-	bookingCollection.FindOneAndUpdate(context.Background(), filter, update, options.FindOneAndUpdate().SetReturnDocument(options.After)).Decode(&booking)
-
-	var event models.Event
-	eventCollection.FindOneAndUpdate(context.Background(), bson.M{"_id": booking.EventID}, bson.M{"$inc": bson.M{"availableSeats": booking.Tickets}}).Decode(&event)
-
+	slog.Info("Successfully release ticket in background")
 }
